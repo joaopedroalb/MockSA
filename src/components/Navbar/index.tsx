438b2947ecb3b00d4808813a4294e4ec4905ec3f
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-10-03 20:11:59 -0300

    Add navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,6 +2,17 @@ import styles from './index.module.scss'
 
 export default function Navbar(){
     return(
-        <div></div>
+        <div className={styles.container}>
+            <h1>Mock <strong>S</strong>A</h1>
+            <ul>
+                <li>SDKs</li>
+                <li>Docs</li>
+                <li>Pricing</li>
+                <li>Company</li>
+                <li>Blog</li>
+                <li>Contact Sales</li>
+                <li><button>Free Trial</button></li>
+            </ul>
+        </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 import styles from '../../styles/Home.module.scss'
+import Navbar from '../components/Navbar'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <Navbar/>
     </div>
   )
 }
