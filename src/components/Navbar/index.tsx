cf25f3073da5b072540597a6635ffa0c86615af1
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-10-03 20:44:09 -0300

    Subindo alteracoes por hoje

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,17 +1,30 @@
 import styles from './index.module.scss'
+import { useState } from 'react'
 
 export default function Navbar(){
+    
+    const [menuActive,setMenuActive] = useState(true);
+
+    // function toggleMenu(){
+        
+    // }
+
     return(
         <div className={styles.container}>
             <h1>Mock <strong>S</strong>A</h1>
-            <ul>
+            <button 
+                className={styles.btnMobile} 
+                onClick={_=>setMenuActive(!menuActive)}
+            >
+                Menu</button>
+            <ul className={menuActive? styles.menuOn:styles.menuOff}>
                 <li>SDKs</li>
                 <li>Docs</li>
                 <li>Pricing</li>
                 <li>Company</li>
                 <li>Blog</li>
                 <li>Contact Sales</li>
-                <li><button>Free Trial</button></li>
+                <li><button className={styles.btnNav}>Free Trial</button></li>
             </ul>
         </div>
     )
