72bcccc2da4f720f384fcbcd30aadbe94e1d8173
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-10-04 09:20:01 -0300

    Update navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,32 +1,33 @@
 import styles from './index.module.scss'
 import { useState } from 'react'
-import useWindowDimensions from '../../Context/useWindowDimensions';
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {HiX} from 'react-icons/hi'
 
 export default function Navbar(){
     
-    const [menuActive,setMenuActive] = useState(true);
-
-    const {width} = useWindowDimensions();
-    // function toggleMenu(){
-        
-    // }
+    const [menuActive,setMenuActive] = useState(false);
 
     return(
-        <div className={width>660? styles.container:menuActive?styles.containerMobileOff:styles.containerMobileOn}>
+        <div className={menuActive?styles.containerMobileOn:styles.container}>
             <h1>Mock <strong>S</strong>A</h1>
             <button 
-                className={menuActive? styles.btnMobile:styles.btnMobileActive} 
+                className={menuActive? styles.btnMobileActive:styles.btnMobile} 
                 onClick={_=>setMenuActive(!menuActive)}
-            >
-                Menu</button>
+            >{menuActive?
+                <HiX color={"#fff"} size={25}/>
+                :
+                <GiHamburgerMenu color={"#fff"} size={25}/>
+            }
+            
+            </button>
             <ul className={menuActive? styles.menuOn:styles.menuOff}>
-                <li>SDKs</li>
-                <li>Docs</li>
-                <li>Pricing</li>
-                <li>Company</li>
-                <li>Blog</li>
-                <li>Contact Sales</li>
-                <li><button className={styles.btnNav}>Free Trial</button></li>
+                <li onClick={_=>setMenuActive(false)}>SDKs</li>
+                <li onClick={_=>setMenuActive(false)}>Docs</li>
+                <li onClick={_=>setMenuActive(false)}>Pricing</li>
+                <li onClick={_=>setMenuActive(false)}>Company</li>
+                <li onClick={_=>setMenuActive(false)}>Blog</li>
+                <li onClick={_=>setMenuActive(false)}>Contact Sales</li>
+                <li><button className={styles.btnNav} onClick={_=>setMenuActive(false)}>Free Trial</button></li>
             </ul>
         </div>
     )
