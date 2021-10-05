da42571af2009d1306cca4d7f1abf1180fd3f1df
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-10-04 21:41:08 -0300

    Update style bottom sec

--- src/components/Corpo/BottomSec/index.tsx
+++ src/components/Corpo/BottomSec/index.tsx
@@ -7,21 +7,31 @@ export default function BottomSec(){
     return(
         <section className={styles.container}>
             <div className={styles.textContainer}>
-                <h1>Giving Back</h1>
-                <span>As tech and design enthusiasts. 
-                    gamers, and creators, we benefit from 
-                    creations and communities that power our world, 
-                    so it is time to give back
-                </span>
-                <h2>DESIGN x TECHNOLOGY (DXT)</h2>
-                <span>
-                    In our annual DXT conference. We gather creatives in various fields. 
-                    from creative coders to branding experts, to discover new trends and open new grounds
-                </span>
-                <h2>Supporting Open source</h2>
-                <span>
-                    Smart code powers ideas, So we love connecting with open Source communities
-                </span>
+
+                <div>
+                    <h1>Giving Back</h1>
+                    <span>As tech and design enthusiasts. 
+                        gamers, and creators, we benefit from 
+                        creations and communities that power our world, 
+                        so it is time to give back
+                    </span>
+                </div>
+
+                <div>
+                    <h5>DESIGN x TECHNOLOGY (DXT)</h5>
+                    <span>
+                        In our annual <strong style={{color:"#06d6a0"}}>DXT conference</strong>. We gather creatives in various fields. 
+                        from creative coders to branding experts, to discover new trends and open new grounds
+                    </span>
+                </div>
+                
+                <div>
+                    <h5>Supporting Open source</h5>
+                    <span>
+                        Smart code powers ideas, So we love connecting with Open Source communities.
+                    </span>
+                </div>
+
             </div>
             <br/>
             <Image src={pairPrograming} alt="Design in work" width={300} height={300}/>
